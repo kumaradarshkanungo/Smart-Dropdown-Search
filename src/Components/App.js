928e53ecb2b0b603d99099b39b8e8f3b5d46325b
Kumar Adarsh Kanungo <kanungo.kumaradarsh@gmail.com>
2020-08-29 16:32:10 +0530

    Initial Revision

---
+++ src/Components/App.js
@@ -0,0 +1,92 @@
+import React, {Component} from 'react';
+import SmartDropDownSearch from './SmartDropDownSearch';
+import Axios from './utils/Axios'
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      loading : true,
+      countryList : [],
+      selectedCountry : '',
+      privilidge : true,
+      noOfItems : 5,
+      loggedInAs: 'admin'
+    }
+  }
+
+  componentDidMount() {
+    this.fnGetCountryList();
+  }
+
+  fnGetCountryList = async () => {
+    try{
+      const result = await Axios.get('/countries');
+      if(result.status !== 200){ // if Response Status is not 200
+        alert("something went wrong! Please try again later.");
+        return false;
+      }
+
+      const countryList = result.data.countries;
+      this.setState({ countryList, loading: false });
+    } catch (e) {
+      alert(`something went wrong! Please try again later.`);
+    }
+  }
+
+  addAndSelectHandler = async (CountryInput, flag) => {
+    if(flag === "Add"){  // In case user clicks on Add Button
+      try{
+        const result = await Axios.get(`/addcountry?name=${CountryInput}`);
+        if(result.status !== 200){ // if Response Status is not 200
+          alert("something went wrong! Please try again later.");
+          return false;
+        }
+        alert("Added Successfully !!!");
+        await this.setState({loading: true})
+        this.fnGetCountryList();
+      }
+      catch(e){
+        if(e.response.status === 500){
+          alert("You are not allowed to add duplicate!!")
+        }
+      }
+    }
+    else if(flag === "Select") { // In case user selects any country
+      this.setState({selectedCountry : CountryInput})
+    }
+  }
+
+  changeUser = val => {
+    const privilidge = (val === 'admin') ? true : false;
+    this.setState({loggedInAs: val, privilidge})
+  }
+
+  render(){
+    const { loading, countryList, selectedCountry, privilidge, noOfItems, loggedInAs } = this.state;
+    
+    if(loading) return '';  // If Page is Loading white screen to be displayed
+    
+    return (
+      <div className="App">
+        <div className="loginSection">
+          Logged In As :
+          <p className={(loggedInAs==='admin')?'active':''}
+            onClick={()=>this.changeUser('admin')}>
+            Admin
+          </p>
+          |
+          <p className={(loggedInAs==='user')?'active':''} 
+            onClick={()=>this.changeUser('user')}>
+            User
+          </p>
+        </div>
+        <SmartDropDownSearch countryList={countryList} privilidge={privilidge} noOfItems={noOfItems} addAndSelectHandler={this.addAndSelectHandler} />
+        <br/>
+        <p>{`Selected Country : ${selectedCountry}`}</p>
+    </div>
+    );
+  }
+}
+
+export default App;
